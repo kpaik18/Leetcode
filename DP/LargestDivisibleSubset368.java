2bef7fda0a54bfdeb6846b5b9a99ba8f78d2765d
Koba.Paikidze <koba.paikidze@ol.ge>
2023-11-20 17:34:56 +0400

    added array utils, started largest divisible subset 368

---
+++ DP/LargestDivisibleSubset368.java
@@ -0,0 +1,15 @@
+package DP;
+
+import java.util.List;
+
+public class LargestDivisibleSubset368 {
+    public static void main(String[] args) {
+        largestDivisibleSubset(new int[]{5, 10, 7, 14, 21, 49});
+    }
+
+    public static List<Integer> largestDivisibleSubset(int[] nums) {
+        return null;
+    }
+
+
+}
---
+++ DP/utils/ArrayUtils.java
@@ -0,0 +1,10 @@
+package DP.utils;
+
+public class ArrayUtils {
+    public static void printArray(int[] nums) {
+        for (var next : nums) {
+            System.out.print(next + " ");
+        }
+        System.out.println();
+    }
+}
