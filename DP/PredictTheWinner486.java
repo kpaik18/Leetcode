69024da4538794498e2b58a086a7dc6e9fc2aff9
Koba.Paikidze <koba.paikidze@ol.ge>
2023-12-13 12:42:31 +0400

    predict the winner 486 done

--- DP/PredictTheWinner486.java
+++ DP/PredictTheWinner486.java
@@ -2,26 +2,55 @@ package DP;
 
 public class PredictTheWinner486 {
     public static void main(String[] args) {
-        System.out.println(predictTheWinner(new int[]{1, 5, 2}));
+        System.out.println(predictTheWinner(new int[]{1, 5, 2, 4, 6}));
     }
 
     static public boolean predictTheWinner(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
+        Cell[][] dp = new Cell[nums.length][nums.length];
+        createDp(dp);
         fillDp(dp, nums);
-        int size = nums.length - 1;
-        if (dp[0][size] >= Math.max(dp[1][size], dp[0][size - 1])) {
-            return true;
+        printCellMatrix(dp);
+        int s = nums.length - 1;
+//        if (dp[0][s].score >= Math.max(dp[1][s].score, dp[0][s - 1].score)) {
+//            return true;
+//        }
+        int myChoice = dp[0][s].score;
+        int choice = dp[0][s].index;
+        if (choice == 0) {
+            return myChoice >= dp[1][s].score;
         }
-        return false;
+        return myChoice >= dp[0][s - 1].score;
     }
 
-    static private void fillDp(int[][] dp, int[] nums) {
+    private static void printCellMatrix(Cell[][] dp) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                int index = dp[i][j].index;
+                boolean left = true;
+                if (index == j) {
+                    left = false;
+                }
+                System.out.print(dp[i][j].score + (left ? "l" : "r") + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void createDp(Cell[][] dp) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = new Cell(0, 0);
+            }
+        }
+    }
+
+    static private void fillDp(Cell[][] dp, int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             fillNlengthSubProblem(dp, nums, i);
         }
     }
 
-    static private void fillNlengthSubProblem(int[][] dp, int[] nums, int len) {
+    static private void fillNlengthSubProblem(Cell[][] dp, int[] nums, int len) {
         for (int i = 0; i < nums.length; i++) {
             int left = i;
             int right = i + len;
@@ -30,17 +59,63 @@ public class PredictTheWinner486 {
         }
     }
 
-    private static void fillDpCell(int[][] dp, int[] nums, int left, int right) {
+    private static void fillDpCell(Cell[][] dp, int[] nums, int left, int right) {
         if (left == right) {
-            dp[left][right] = nums[left];
+            dp[left][right].score = nums[left];
+            dp[left][right].index = left;
             return;
         }
         if (right - left == 1) {
-            dp[left][right] = Math.max(nums[left], nums[right]);
+            int max = Math.max(nums[left], nums[right]);
+            dp[left][right].score = max;
+            boolean isLeft = true;
+            if (nums[right] > nums[left]) {
+                isLeft = false;
+            }
+            if (isLeft) {
+                dp[left][right].index = left;
+            } else {
+                dp[left][right].index = right;
+            }
             return;
         }
-        int leftChoiceEnemyWin = dp[left + 1][right];
-        int rightChoiceEnemyWin = dp[left][right - 1];
+        int leftChoice = nums[left];
+        int rightChoice = nums[right];
 
+        int enemyLeftIndex = dp[left + 1][right].index;
+        int enemyRightIndex = dp[left][right - 1].index;
+
+        int leftOptimalMax = 0;
+        if (enemyLeftIndex == left + 1) {
+            leftOptimalMax = dp[left + 2][right].score;
+        } else {
+            leftOptimalMax = dp[left + 1][right - 1].score;
+        }
+        int rightOptimalMax = 0;
+        if (enemyRightIndex == left) {
+            rightOptimalMax = dp[left + 1][right - 1].score;
+        } else {
+            rightOptimalMax = dp[left][right - 2].score;
+        }
+        int fullLeft = leftChoice + leftOptimalMax;
+        int fullRight = rightChoice + rightOptimalMax;
+        if (fullLeft > fullRight) {
+            dp[left][right].score = fullLeft;
+            dp[left][right].index = left;
+        } else {
+            dp[left][right].score = fullRight;
+            dp[left][right].index = right;
+        }
+
+    }
+
+    static class Cell {
+        int index;
+        int score;
+
+        public Cell(int index, int score) {
+            this.index = index;
+            this.score = score;
+        }
     }
 }
