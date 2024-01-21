b2c9339e2e1df7d12bdfabc9472d049766101b48
Koba.Paikidze <koba.paikidze@ol.ge>
2024-01-21 16:11:47 +0400

    not done coin change 518

---
+++ DP/CoinChangeII518.java
@@ -0,0 +1,41 @@
+package DP;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CoinChangeII518 {
+    public static void main(String[] args) {
+        System.out.println(change(500, new int[]{1, 2, 5}));
+    }
+
+    public static int change(int amount, int[] coins) {
+        coins = Arrays.stream(coins).sorted().toArray();
+        int result = helper(amount, coins, 0);
+        return result;
+    }
+
+    private static int helper(int amount, int[] coins, int minIndex) {
+        if (amount < 0) {
+            return 0;
+        }
+        if (amount == 0) {
+            return 1;
+        }
+        int result = 0;
+        for (int i = minIndex; i < coins.length; i++) {
+            int next = coins[i];
+            result += helper(amount - next, coins, i);
+        }
+        return result;
+
+    }
+
+    private static void printList(List<Integer> chosen) {
+        for (var next : chosen) {
+            System.out.print(next + " ");
+        }
+        System.out.println();
+    }
+
+
+}
